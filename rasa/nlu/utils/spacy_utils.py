c94a06eda0c8f6f79da10d0840ff7ad9686af20b
Joseph Juzl <joejuzl@gmail.com>
2021-09-03 09:56:37 +0200

    wip

--- rasa/nlu/utils/spacy_utils.py
+++ rasa/nlu/utils/spacy_utils.py
@@ -5,11 +5,15 @@ import dataclasses
 from pathlib import Path
 import typing
 import logging
-from typing import Any, Dict, List, Optional, Text
+from typing import Any, Dict, List, Optional, Text, Tuple
+
 
 from rasa.engine.graph import ExecutionContext, GraphComponent
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
+from rasa.nlu.constants import DENSE_FEATURIZABLE_ATTRIBUTES, SPACY_DOCS
+from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.training_data.training_data import TrainingData
 import rasa.utils.train_utils
 from rasa.nlu.model import InvalidModelError
 from rasa.shared.constants import DOCS_URL_COMPONENTS
@@ -18,6 +22,7 @@ logger = logging.getLogger(__name__)
 
 if typing.TYPE_CHECKING:
     from spacy.language import Language  # noqa: F401
+    from spacy.tokens import Doc
 
 # TODO: This is a workaround around until we have all components migrated to
 # `GraphComponent`.
@@ -28,11 +33,7 @@ SpacyNLP = rasa.nlu.utils._spacy_utils.SpacyNLP
 class SpacyModel:
     """Wraps `SpacyNLPGraphComponent` output to make it fingerprintable."""
 
-    def __init__(
-        self,
-        model: "Language",
-        model_path: Path,
-    ) -> None:
+    def __init__(self, model: Language, model_path: Path,) -> None:
         """Initializing SpacyModel."""
         self.model = model
         self.model_path = model_path
@@ -56,28 +57,13 @@ class SpacyNLPGraphComponent(GraphComponent):
     model is only loaded once and then shared by depending components.
     """
 
-    @staticmethod
-    def get_default_config() -> Dict[Text, Any]:
-        return {
-            # when retrieving word vectors, this will decide if the casing
-            # of the word is relevant. E.g. `hello` and `Hello` will
-            # retrieve the same vector, if set to `False`. For some
-            # applications and models it makes sense to differentiate
-            # between these two words, therefore setting this to `True`.
-            "case_sensitive": False,
-        }
-
-    def __init__(
-        self,
-        model: "Language" = None,
-        model_name: Text = None,
-    ) -> None:
+    def __init__(self, model: Language = None, model_name: Text = None,) -> None:
         """Initializes a `SpacyNLPGraphComponent`."""
-        self.model = model
-        self.model_name = model_name
+        self._model = model
+        self._model_name = model_name
 
     @staticmethod
-    def load_model(spacy_model_name: Text) -> "Language":
+    def load_model(spacy_model_name: Text) -> Language:
         """Try loading the model, catching the OSError if missing."""
         import spacy
 
@@ -126,7 +112,7 @@ class SpacyNLPGraphComponent(GraphComponent):
         return cls(model, spacy_model_name)
 
     @staticmethod
-    def ensure_proper_language_model(nlp: Optional["Language"]) -> None:
+    def ensure_proper_language_model(nlp: Optional[Language]) -> None:
         """Checks if the spacy language model is properly loaded.
 
         Raises an exception if the model is invalid."""
@@ -146,3 +132,186 @@ class SpacyNLPGraphComponent(GraphComponent):
                 f"correct model (https://spacy.io/docs/usage/)."
                 ""
             )
+
+
+class SpacyPreprocessor(GraphComponent):
+    """The core component that links spaCy to related components in the pipeline."""
+
+    @staticmethod
+    def get_default_config() -> Dict[Text, Any]:
+        return {
+            # when retrieving word vectors, this will decide if the casing
+            # of the word is relevant. E.g. `hello` and `Hello` will
+            # retrieve the same vector, if set to `False`. For some
+            # applications and models it makes sense to differentiate
+            # between these two words, therefore setting this to `True`.
+            "case_sensitive": False,
+        }
+
+    def __init__(
+        self, config: Dict[Text, Any], model_storage: ModelStorage, resource: Resource,
+    ) -> None:
+        """Constructs a new intent classifier using the MITIE framework."""
+        self._config = config
+        self._model_storage = model_storage
+        self._resource = resource
+
+    @classmethod
+    def required_packages(cls) -> List[Text]:
+        return ["spacy"]
+
+    @classmethod
+    def create(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> SpacyPreprocessor:
+        """Creates component for training see parent class for full docstring)."""
+        return cls(config, model_storage, resource)
+
+    def doc_for_text(self, model: Language, text: Text) -> Doc:
+        """Makes a spaCy doc object from a string of text."""
+        return model(self.preprocess_text(text))
+
+    def preprocess_text(self, text: Optional[Text]) -> Text:
+        """Processes the text before it is handled by spaCy."""
+        if text is None:
+            # converted to empty string so that it can still be passed to spacy.
+            # Another option could be to neglect tokenization of the attribute of
+            # this example, but since we are processing in batch mode, it would
+            # get complex to collect all processed and neglected examples.
+            text = ""
+        if self._config.get("case_sensitive"):
+            return text
+        else:
+            return text.lower()
+
+    def get_text(self, example: Dict[Text, Any], attribute: Text) -> Text:
+        return self.preprocess_text(example.get(attribute))
+
+    @staticmethod
+    def merge_content_lists(
+        indexed_training_samples: List[Tuple[int, Text]],
+        doc_lists: List[Tuple[int, Doc]],
+    ) -> List[Tuple[int, Doc]]:
+        """Merge lists with processed Docs back into their original order."""
+
+        dct = dict(indexed_training_samples)
+        dct.update(dict(doc_lists))
+        return sorted(dct.items())
+
+    @staticmethod
+    def filter_training_samples_by_content(
+        indexed_training_samples: List[Tuple[int, Text]]
+    ) -> Tuple[List[Tuple[int, Text]], List[Tuple[int, Text]]]:
+        """Separates empty training samples from content bearing ones."""
+
+        docs_to_pipe = list(
+            filter(
+                lambda training_sample: training_sample[1] != "",
+                indexed_training_samples,
+            )
+        )
+        empty_docs = list(
+            filter(
+                lambda training_sample: training_sample[1] == "",
+                indexed_training_samples,
+            )
+        )
+        return docs_to_pipe, empty_docs
+
+    def process_content_bearing_samples(
+        self, model: Language, samples_to_pipe: List[Tuple[int, Text]]
+    ) -> List[Tuple[int, Doc]]:
+        """Sends content bearing training samples to spaCy's pipe."""
+
+        docs = [
+            (to_pipe_sample[0], doc)
+            for to_pipe_sample, doc in zip(
+                samples_to_pipe,
+                [
+                    doc
+                    for doc in model.pipe(
+                        [txt for _, txt in samples_to_pipe], batch_size=50
+                    )
+                ],
+            )
+        ]
+        return docs
+
+    def process_non_content_bearing_samples(
+        self, model: Language, empty_samples: List[Tuple[int, Text]]
+    ) -> List[Tuple[int, Doc]]:
+        """Creates empty Doc-objects from zero-lengthed training samples strings."""
+
+        from spacy.tokens import Doc
+
+        n_docs = [
+            (empty_sample[0], doc)
+            for empty_sample, doc in zip(
+                empty_samples, [Doc(model.vocab) for doc in empty_samples]
+            )
+        ]
+        return n_docs
+
+    def docs_for_training_data(
+        self, model: Language, training_data: TrainingData
+    ) -> Dict[Text, List[Any]]:
+        attribute_docs = {}
+        for attribute in DENSE_FEATURIZABLE_ATTRIBUTES:
+
+            texts = [
+                self.get_text(e, attribute) for e in training_data.training_examples
+            ]
+            # Index and freeze indices of the training samples for preserving the order
+            # after processing the data.
+            indexed_training_samples = [(idx, text) for idx, text in enumerate(texts)]
+
+            samples_to_pipe, empty_samples = self.filter_training_samples_by_content(
+                indexed_training_samples
+            )
+
+            content_bearing_docs = self.process_content_bearing_samples(
+                model, samples_to_pipe
+            )
+
+            non_content_bearing_docs = self.process_non_content_bearing_samples(
+                model, empty_samples
+            )
+
+            attribute_document_list = self.merge_content_lists(
+                indexed_training_samples,
+                content_bearing_docs + non_content_bearing_docs,
+            )
+
+            # Since we only need the training samples strings,
+            # we create a list to get them out of the tuple.
+            attribute_docs[attribute] = [doc for _, doc in attribute_document_list]
+        return attribute_docs
+
+    def train(self, training_data: TrainingData, spacy_model: SpacyModel,) -> None:
+
+        model = spacy_model.model
+
+        attribute_docs = self.docs_for_training_data(model, training_data)
+
+        for attribute in DENSE_FEATURIZABLE_ATTRIBUTES:
+
+            for idx, example in enumerate(training_data.training_examples):
+                example_attribute_doc = attribute_docs[attribute][idx]
+                if len(example_attribute_doc):
+                    # If length is 0, that means the initial text feature
+                    # was None and was replaced by ''
+                    # in preprocess method
+                    example.set(SPACY_DOCS[attribute], example_attribute_doc)
+
+    def process(self, model: Language, messages: List[Message]) -> None:
+        for message in messages:
+            for attribute in DENSE_FEATURIZABLE_ATTRIBUTES:
+                if message.get(attribute):
+                    message.set(
+                        SPACY_DOCS[attribute],
+                        self.doc_for_text(model, message.get(attribute)),
+                    )
